f026855219556c706ba2b4901531d320d93a65d3
jingsuowang <1447850489@qq.com>
2023-04-10 17:39:55 +0800

    背景css修改 背景更改事件

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-window.addEventListener('load',()=>{
+window.addEventListener('load',function(){
   b1 = document.getElementById('bacBut');
   bd = document.getElementById('bac');
-  ba = document.getElementsByClassName('bac');
-
+  f1 = document.querySelector('#fileInput');
   b1.addEventListener('click',()=>{
-    bd.style.backgroundImage = 'url(/beachand2.png)';
-    bd.style.width = '100%';
-    bd.style.height = '100%';
+    // bd.style.backgroundImage = 'url(/beachand2.png)';
+    // bd.style.width = '100%';
+    // bd.style.height = '100%';
     console.log('1');
+    f1.click();
   });
+
+  f1.addEventListener('change',function(){
+    var reader = new FileReader();
+    reader.addEventListener('load',(e)=>{
+      console.log(22);
+      console.log(e);
+      bd.style.backgroundImage = "url('" + e.target.result + "')";
+    });
+    reader.readAsDataURL(this.files[0]);
+    console.log(reader);
+  });
+
 });
